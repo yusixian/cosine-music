169bcecef0b8e03801bd8797671ece70b8386c49
yusixian <3450414733@qq.com>
2023-05-03 04:12:10 +0800

    feat: login dialog

---
+++ components/user/LoginDialog.tsx
@@ -0,0 +1,9 @@
+import Dialog from '@/components/dialog';
+import { userLoginDialogAtom } from '@/store/user/state';
+import { useRecoilState } from 'recoil';
+import Login from './Login';
+
+export default function LoginDialog() {
+  const [open, setOpen] = useRecoilState(userLoginDialogAtom);
+  return <Dialog showCloseButton className="p-8" open={open} onOpenChange={setOpen} render={({}) => <Login />} />;
+}
